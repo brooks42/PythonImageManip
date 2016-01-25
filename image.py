dcce61c338aae7fc6c336a7322fe52083769ff78
Chris Brooks <cbrooks@iwsinc.com>
2016-01-25 15:26:27 -0800

    Some cleanup of the file structure so separate 'projects' are in separate folders

--- image.py
+++
@@ -1,9 +0,0 @@
-import Image
-import sys
-
-
-width = sys.argv[1]
-height = sys.argv[2]
-print(" width: " + str(width) + " height: " + str(height))
-image = Image.new("RGB", (int(width), int(height)), "black")
-image.save("image" + width + "x" + height + ".png", None)
